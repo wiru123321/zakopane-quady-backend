64818b0327be05dd06e9e8fc09dd96466141fdd9
wiru123321 <wojtekmaj2@o2.pl>
2025-01-30 21:36:32 +0100

    small changes2

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ const cronTasks = require('@webbio/strapi-plugin-scheduler/cron-task');
 
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1063),
   app: {
     keys: env.array('APP_KEYS', ['testKey1', 'testKey2']),
   },
